3fde5f30be97aa635a1f14a0961f01b15e78ff31
C-Anirudh <canirudh16@gmail.com>
2019-08-04 11:27:42 +0530

    add sign up form

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView, contactView, faqView, error404View *views.View
+var homeView, contactView, faqView, error404View, signupView *views.View
 
 func init() {
 	log.SetPrefix("LOG: ")
@@ -32,6 +32,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 func error404(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	must(error404View.Render(w, nil))
@@ -49,11 +54,13 @@ func main() {
 	contactView = views.NewView("base", "views/contact.gohtml")
 	faqView = views.NewView("base", "views/faq.gohtml")
 	error404View = views.NewView("base", "views/error404.gohtml")
+	signupView = views.NewView("base", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	r.NotFoundHandler = err404
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
