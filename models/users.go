55819f258f48827b10c143b349cd4e5653dc13bc
C-Anirudh <canirudh16@gmail.com>
2019-08-08 21:36:05 +0530

    improve search by id and implement search by email function

--- models/users.go
+++ models/users.go
@@ -46,19 +46,40 @@ func (us *UserService) Close() error {
 // ByID is used to search a user by ID from the db
 func (us *UserService) ByID(id uint) (*User, error) {
 	var user User
-	err := us.db.Where("id = ?", id).First(&user).Error
-	switch err {
-	case nil:
-		return &user, nil
-	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
-	default:
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	if err != nil {
 		return nil, err
 	}
+	return &user, nil
 }
 
 // DestructiveReset drops the user table and rebuilds it
 func (us *UserService) DestructiveReset() {
 	us.db.DropTableIfExists(&User{})
 	us.db.AutoMigrate(&User{})
+}
+
+// Create is used to add a new user
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	}
+	return nil
+}
+
+// ByEmail is used to search a user by email from the db
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email = ?", email)
+	err := first(db, &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
